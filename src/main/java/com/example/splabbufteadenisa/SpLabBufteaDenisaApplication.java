7c107c7a488ce13b29263c205ebabcd8858686ac
DeniBD <denisa.buftea02@e-uvt.ro>
2023-10-17 20:21:38 +0300

    Project Setup

---
+++ src/main/java/com/example/splabbufteadenisa/SpLabBufteaDenisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.splabbufteadenisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLabBufteaDenisaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLabBufteaDenisaApplication.class, args);
+    }
+
+}
